fc478ce259fd552d39929c6bd4e1cb0a8124ecf8
jambottle <jaewon.walks@gmail.com>
2022-01-27 23:30:41 +0900

    feat: 상품 옵션 영역 내에 Input 컴포넌트 구현

---
+++ src/components/organisms/ProductOption/Input/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import DeleteButton from 'components/atoms/DeleteButton';
+import { InputWrapper } from './style';
+
+export default function ProductOptionInput() {
+  return (
+    <InputWrapper>
+      <div className="option-delete">
+        <DeleteButton />
+      </div>
+
+      <div className="option-name">
+        <input type="text" placeholder="옵션명을 입력해 주세요. (필수)" />
+      </div>
+
+      <div className="option-info">
+        <input className="price" type="text" placeholder="상품 정상가 (필수)" />원
+        <span>할인율 %</span>
+        <input className="price" type="text" placeholder="상품 판매가 (필수)" />원
+        <input className="stock" type="text" placeholder="재고 (필수)" />개
+        <select name="tax">
+          <option value="non-taxable">비과세</option>
+          <option value="taxable">과세</option>
+        </select>
+      </div>
+
+      <div className="option-info appended">
+        <span>└</span>
+        <input className="name" type="text" placeholder="추가 옵션명 (필수)" />
+        <input className="price" type="text" placeholder="추가 옵션 정상가 (필수)" />원
+        <DeleteButton />
+      </div>
+
+      <div className="option-append">
+        <button type="button">+</button> 추가 옵션 상품 추가
+      </div>
+    </InputWrapper>
+  );
+}
---
+++ src/components/organisms/ProductOption/Input/style.jsx
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+
+export const InputWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  margin: 12px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 12px;
+  background-color: #fff;
+  font-size: 16px;
+
+  div {
+    margin-bottom: 15px;
+  }
+
+  .option-delete {
+    margin-left: auto;
+
+    button {
+      padding: 6px 16px;
+      font-size: 15px;
+    }
+  }
+
+  .option-name input {
+    width: calc(100% - 20px);
+    height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding-left: 16px;
+    font-size: 16px;
+  }
+
+  .option-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 15px;
+
+    input {
+      height: 40px;
+      margin-right: -10px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding-left: 16px;
+      font-size: 15px;
+
+      &.price {
+        width: 140px;
+      }
+
+      &.stock {
+        width: 80px;
+      }
+    }
+
+    select {
+      height: 40px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding: 0 6px;
+      font-size: 15px;
+    }
+
+    &.appended {
+      span {
+        margin-right: -10px;
+        color: #ddd;
+        font-size: 36px;
+      }
+
+      input {
+        height: 40px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        padding-left: 16px;
+        font-size: 15px;
+
+        &.name {
+          width: 300px;
+        }
+
+        &.price {
+          width: 200px;
+          margin-right: -10px;
+        }
+      }
+
+      button {
+        height: 40px;
+        padding: 6px 16px;
+        font-size: 15px;
+      }
+    }
+  }
+
+  .option-append {
+    display: flex;
+    align-items: center;
+
+    button {
+      width: 32px;
+      height: 32px;
+      margin-right: 12px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding: 0;
+      background-color: #fff;
+      font-size: 29px;
+      cursor: pointer;
+    }
+  }
+`;
