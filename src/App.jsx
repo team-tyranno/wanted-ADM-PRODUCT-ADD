977a08fb1d473de5729b5ddd36cae001340d1a19
jambottle <jaewon.walks@gmail.com>
2022-01-28 00:30:21 +0900

    feat: App.jsx에 Image/Input 컴포넌트 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Category } from './components/templates/Category';
-import { Item } from './components/templates/Item';
+import Category from 'components/templates/Category';
+import Item from 'components/templates/Item';
+import ProductOptionImage from 'components/organisms/ProductOption/Image';
+import ProductOptionInput from 'components/organisms/ProductOption/Input';
 
 function App() {
   return (
-    <Category title="상품 기본 정보">
-      <Item title="카테고리">
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-      </Item>
-    </Category>
+    <>
+      <Category title="상품 기본 정보">
+        <Item title="카테고리">
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+        </Item>
+      </Category>
+      <Category title="상품 옵션*">
+        <ProductOptionImage />
+        <ProductOptionInput />
+      </Category>
+    </>
   );
 }
 
