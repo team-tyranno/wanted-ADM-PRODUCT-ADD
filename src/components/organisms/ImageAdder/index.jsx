ed2bede91762556d0ae4391374840aa5828c8922
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-01-28 14:06:08 +0900

    fix: 컴포넌트 이름 수정에 따른 변경

--- src/components/organisms/ImageAdder/index.jsx
+++ src/components/organisms/ImageAdder/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { ImageInput, FileList } from 'components';
+import { InputImage, FileList } from 'components';
 import * as S from './style';
 
 export function ImageAdder() {
   return (
     <S.Container>
-      <ImageInput title="+ 이미지 추가" />
+      <InputImage title="+ 이미지 추가" />
       <FileList />
     </S.Container>
   );
