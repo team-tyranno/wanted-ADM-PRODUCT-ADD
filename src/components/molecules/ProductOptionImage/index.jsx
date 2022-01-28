317a08d89f46ab4f4ab85c836dd241b62651f44f
jambottle <jaewon.walks@gmail.com>
2022-01-29 06:45:34 +0900

    rename: ImageUploader 컴포넌트 이름 변경 (→ ProductOptionImage) - 외부 스타일 시트의 import 방식 통일 및 스타일 시트 내의 불필요한 주석 삭제

---
+++ src/components/molecules/ProductOptionImage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { InputImage } from 'components';
+import * as S from './style';
+
+export function ProductOptionImage() {
+  return (
+    <S.Wrapper>
+      <InputImage />
+    </S.Wrapper>
+  );
+}
