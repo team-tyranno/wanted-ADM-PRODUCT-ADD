2e8993a4a58b0decb45670ff07da75096dfb5469
jambottle <jaewon.walks@gmail.com>
2022-01-28 09:25:22 +0900

    refactor: 현재의 컴포넌트 분류에 따라 components/index.js 수정 - atoms, organisms, templates의 세 단계에 맞춰 컴포넌트의 연결을 구분하여 정리

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,20 @@
-export { Input } from './atoms/Input';
-export { Button } from './atoms/Button';
-export { ButtonSwitch } from 'components/atoms/ButtonSwitch';
+// atoms
+export { AppendButton } from 'components/atoms/AppendButton';
+export { Button } from 'components/atoms/Button';
 export { ButtonRadio } from 'components/atoms/ButtonRadio';
+export { DeleteButton } from 'components/atoms/DeleteButton';
+export { ImageUploader } from 'components/atoms/ImageUploader';
+export { Input } from './atoms/Input';
 export { InputDate } from 'components/atoms/InputDate';
 
+// organisms
 export { ButtonRadioGroup } from 'components/organisms/ButtonRadioGroup';
+export { GoodsName } from 'components/organisms/GoodsName';
 export { InputDatePeriod } from 'components/organisms/InputDatePeriod';
-export { GoodsName } from './organisms/GoodsName';
+export { ProductOption } from 'components/organisms/ProductOption';
 
-
+// templates
 export { Category } from 'components/templates/Category';
 export { Item } from 'components/templates/Item';
-export { SalesExposedPeriod } from 'components/templates/SalesExposedPeriod';
 export { ItemWithTwoCol } from './templates/ItemWithTwoCol/index';
+export { SalesExposedPeriod } from 'components/templates/SalesExposedPeriod';
