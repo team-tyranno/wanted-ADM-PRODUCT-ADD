7313aecaabbae474a4566b3179a7195cbdf49a50
gml9812 <hee.jun.yun@gmail.com>
2022-01-28 17:57:35 +0900

    refactor: InputDatePeriod, InputDate 더 범용적으로 사용 가능하도록 수정

--- src/components/molecules/InputDatePeriod/index.jsx
+++ src/components/molecules/InputDatePeriod/index.jsx
@@ -4,17 +4,24 @@ import { InputDate } from 'components';
 
 import * as S from './style';
 
-export function InputDatePeriod({ nameAndCallback }) {
+export function InputDatePeriod({ nameAndCallback, description }) {
   return (
     <S.Container>
-      {nameAndCallback.map((el) => (
-        <InputDate name={el.name} onChange={el.onChange} />
+      {nameAndCallback.map((el, index) => (
+        <>
+          {index === 0 && description !== '' && (
+            <S.Description htmlFor={el.name}>{description}</S.Description>
+          )}
+          {index !== 0 && <S.Tilde>~</S.Tilde>}
+          <InputDate name={el.name} date={el.date} onChange={el.onChange} />
+        </>
       ))}
     </S.Container>
   );
 }
 
 InputDatePeriod.propTypes = {
+  description: PropTypes.string.isRequired,
   nameAndCallback: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
