5061ee3a866a14635b1561d3ab1162261be97f85
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-01-29 14:14:54 +0900

    remove: 사용하지 않는코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { GoodsInformation, GoodsStock, Item } from 'components';
 
 function App() {
   return (
     <>
-      <Item>
-        <GoodsInformation />
-      </Item>
-      <Item>
-        <GoodsStock />
-      </Item>
       <div>hello</div>
       <div>hello</div>
     </>
