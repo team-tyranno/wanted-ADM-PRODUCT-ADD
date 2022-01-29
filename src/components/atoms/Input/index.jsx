4195cfb0d1e5bdf07c79627c003406827e2bd39d
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-01-29 16:44:02 +0900

    fix: Input 태그 width prop 추가

--- src/components/atoms/Input/index.jsx
+++ src/components/atoms/Input/index.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Inp } from './style';
 
-export function Input({ placeHolder, onChange, onFocus, onBlur }) {
+export function Input({ width, placeHolder, onChange, onFocus, onBlur }) {
   return (
-    <Container>
+    <Container width={width}>
       <Inp placeholder={placeHolder} onChange={onChange} onFocus={onFocus} onBlur={onBlur} />
     </Container>
   );
 }
 
 Input.defaultProps = {
+  width: null,
   onChange: null,
   onFocus: null,
   onBlur: null,
 };
 
 Input.propTypes = {
+  width: PropTypes.string,
   placeHolder: PropTypes.string.isRequired,
   onChange: PropTypes.oneOfType([null, PropTypes.func]),
   onFocus: PropTypes.oneOfType([null, PropTypes.func]),
